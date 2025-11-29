e84b5495e6d8e578759faa1b41e7ce2fc5ed0733
zaltys <povilas.simanskas@gmail.com>
2025-11-29 11:58:24 +0200

    bools, numbers

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,21 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / 100
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	groups := uint(carsCount / 10)
+	remainder := uint(carsCount % 10)
+
+	return groups*95000 + remainder*10000
+}
--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,10 +1,14 @@
+// Package weather is for weather.
 package weather
 
 var (
+	//CurrentCondition definition.
 	CurrentCondition string
-	CurrentLocation  string
+	//CurrentLocation definition.
+	CurrentLocation string
 )
 
+// Forecast Function that forecasts.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
